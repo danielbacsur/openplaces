672fe021968bf784d17231e3b8f6e10c84c11bb3
Daniel Bacsur <contact@danielbacsur.com>
2026-05-13 17:04:03 +0000

    test: update vitest configuration with new timeouts

--- typescript/vitest.config.ts
+++ typescript/vitest.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
-    hookTimeout: 20_000,
+    hookTimeout: 20000,
+    testTimeout: 20000,
+    slowTestThreshold: 10000,
+    reporters: ["tree"],
   },
 });
