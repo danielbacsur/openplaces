1f03d811e2676dc4837c9bff8c825a2c815e6428
Daniel Bacsur <contact@danielbacsur.com>
2026-05-17 16:47:12 +0200

    chore: hoist tsconfigs and add import path aliases

--- openplaces/vitest.config.ts
+++ openplaces/vitest.config.ts
@@ -1,6 +1,14 @@
+import { fileURLToPath } from "node:url";
+
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./openplaces", import.meta.url)),
+    },
+  },
+
   test: {
     reporters: ["tree"],
   },
