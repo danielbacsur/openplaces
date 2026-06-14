680e75667688875261a8db6983465a537778f7b7
Daniel Bacsur <contact@danielbacsur.com>
2026-06-14 19:11:08 +0000

    chore: clean up files created by create-next-app

---
+++ openplaces-atlas/app/metadata.ts
@@ -0,0 +1,7 @@
+import { type Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "OpenPlaces",
+  description:
+    "Look up places, businesses, addresses, and location metadata without a proprietary places API.",
+};
