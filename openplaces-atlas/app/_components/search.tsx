d667e4522aee0d4bd8bc75cfd82af54da5b8d734
Daniel Bacsur <contact@danielbacsur.com>
2026-06-16 14:57:12 +0000

    feat: add the search bar to the results side panel

---
+++ openplaces-atlas/app/_components/search.tsx
@@ -0,0 +1,22 @@
+export function Search() {
+  return (
+    <form
+      className="relative z-15 my-3 ml-4 box-border h-12 w-[376px] rounded-3xl border border-[#e3e3e3] bg-white py-[11px] pr-[106px] pl-6"
+      spellCheck={false}
+    >
+      <input
+        className="h-6 w-full bg-transparent font-sans text-[15px] leading-6 tracking-[0.1px] text-[#1f1f1f] outline-none placeholder:text-[#5e5e5e]"
+        placeholder="Search OpenPlaces"
+        autoComplete="off"
+      />
+
+      <button className="absolute top-1/2 right-[54px] -translate-y-1/2 cursor-pointer p-3 text-[#5e5e5e]">
+        <span className="material-symbols-outlined block">search</span>
+      </button>
+
+      <button className="absolute top-1/2 right-0 -translate-y-1/2 cursor-pointer px-[15px] py-3 text-[#5e5e5e]">
+        <span className="material-symbols-outlined block">close</span>
+      </button>
+    </form>
+  );
+}
--- openplaces-atlas/app/page.tsx
+++ openplaces-atlas/app/page.tsx
@@ -1,9 +1,14 @@
 import { Aside } from "./_components/aside";
+import { Search } from "./_components/search";
 
 export default function Page() {
   return (
     <main className="relative flex h-dvh w-dvw overflow-hidden bg-[rgb(232,234,237)]">
       <Aside />
+
+      <div className="w-[408px] bg-white">
+        <Search />
+      </div>
     </main>
   );
 }
