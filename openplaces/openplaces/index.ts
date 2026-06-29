61a21471220937d38a0aa863824ac430deabec0d
Daniel Bacsur <contact@danielbacsur.com>
2026-06-29 19:42:04 +0000

    feat: load observer plugins from the client options

--- openplaces/openplaces/index.ts
+++ openplaces/openplaces/index.ts
@@ -6,9 +6,21 @@ import { Places } from "./places";
 export class OpenPlaces extends EventEmitter<OpenPlaces.Events> {
   static readonly Places = Places;
   readonly places = new Places(this);
+
+  constructor({ ...plugins }: OpenPlaces.Options = {}) {
+    super();
+
+    for (const plugin of Object.values(plugins)) {
+      if (typeof plugin === "function") plugin(this);
+    }
+  }
 }
 
 export namespace OpenPlaces {
+  export interface Options {}
+
+  export type Plugin = (client: OpenPlaces) => void;
+
   export type Events = {
     query: [query: string];
     place: [place: Place];
