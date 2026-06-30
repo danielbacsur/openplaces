d34675a64917466dc99af406ee419e518c0c7674
Daniel Bacsur <contact@danielbacsur.com>
2026-06-30 13:47:21 +0000

    feat: stream live places through a useatlas hook

---
+++ openplaces-website/app/_hooks/use-atlas.ts
@@ -0,0 +1,33 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
+import { type Place } from "openplaces";
+
+type Event = { type: "query"; query: string } | { type: "place"; place: Place };
+type State = { query: string | null; places: Place[] };
+
+export function useAtlas(): State {
+  const [state, setState] = useState<State>({ query: null, places: [] });
+
+  useEffect(() => {
+    const source = new EventSource("http://127.0.0.1:28527");
+    const ids = new Map<string, Place>();
+
+    source.onmessage = (message) => {
+      const event: Event = JSON.parse(message.data);
+
+      if (event.type === "query") {
+        ids.clear();
+        setState({ query: event.query, places: [] });
+      } else {
+        ids.set(event.place.id, event.place);
+        setState((s) => ({ ...s, places: [...ids.values()] }));
+      }
+    };
+
+    return () => source.close();
+  }, []);
+
+  return state;
+}
